77e87f45714c14b99f8410b5e3a732616cfd52a0
alba.duque <alba.duque@106-07.info2>
2025-03-17 10:41:57 +0100

    Merge branch 'master' of https://github.com/ivancarrascosa/Minesweeper.git main

--- src/minesweeper/MinesweeperMain.java
+++ src/minesweeper/MinesweeperMain.java
@@ -1,9 +1,18 @@
 package minesweeper;
 
+import java.util.Scanner;
+
 public class MinesweeperMain {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner (System.in);
+		
+		Minesweeper player;
+		
+		
+		
+		sc.close();
 
 	}
 
