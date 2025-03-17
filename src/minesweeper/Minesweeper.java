07adca096dc0cfe00abb27d791d9b8f1250d6aad
ivan.carrascosa <ivan.carrascosa@106-06.info2>
2025-03-17 10:40:56 +0100

    Method printBoard

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -4,19 +4,29 @@ public class Minesweeper {
 	static final int N = 7;
 	static final int M = 7;
 	
-	char[][] playerBoard = new char[N][M];
+	static char[][] playerBoard = new char[N][M];
 	
-	char[][] fullBoard = new char[N][M];
+	static char[][] fullBoard = new char[N][M];
 	
 	int numberOfMines = 10;
 	
-
+	
 
 
 	public static void printBoard() {
+		char column = 'A';
 		for(int i = 1 ; i <= M ; i++) {
 			System.out.print("\t" + i);
 		}
+		for (int i = 0 ; i < N ; i++) {
+			System.out.print(column + "\t");
+			for (int j = 0 ; j < M ; j++) {
+				System.out.print(playerBoard[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+		
+		
 	}
 	
 	
