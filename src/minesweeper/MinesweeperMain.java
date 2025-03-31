77599d5bfe5df53da2995df161b9a6e9ed554586
sgonzalez <sgonzalez@106-03.info2>
2025-03-31 11:04:51 +0200

    1

--- src/minesweeper/MinesweeperMain.java
+++ src/minesweeper/MinesweeperMain.java
@@ -1,12 +1,13 @@
 package minesweeper;
 
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Scanner;
 
 public class MinesweeperMain {
 
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner (System.in);
 
 		boolean boom = false;
 		Minesweeper.printBoard();
@@ -14,22 +15,32 @@ public class MinesweeperMain {
 		Minesweeper.printBoard();
 		Minesweeper.lookForMine(0, 0);
 		Minesweeper.printBoard();
-		while(boom) {
-			
-			
-			
-		Minesweeper.printBoard();
-		
-		
-		
-		
+		while (boom) {
+
+			Minesweeper.printBoard();
+
 		}
-		
-		
-		
-		
+
 		sc.close();
 
+	}
+
+	public static char askCoordinateX() {
+		char charX;
+
+		System.out.println("Enter rows");
+		charX = sc.nextLine().toUpperCase().charAt(0);
+
+		return charX;
+	}
+
+	public static int askCoordinateY() {
+		int y;
+
+		System.out.println("Enter columnns");
+		y = sc.nextInt();
+
+		return y;
 	}
 
 }
